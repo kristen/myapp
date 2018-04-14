39b761ffd96fcea9b8135e39afa462f0118d5d6b
Kristen Sundquist <kssundquist@gmail.com>
2018-04-14 12:59:03 -0700

    Add dependencies

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Myapp.MixProject do
     [
       app: :myapp,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.6.1",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,6 +21,8 @@ defmodule Myapp.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:cowboy, "~> 2.3"},
+      {:plug, "~> 1.5"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
